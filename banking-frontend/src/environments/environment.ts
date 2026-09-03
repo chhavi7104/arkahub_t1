72ce5ed90beb92b714966c6e10cd912a4b316b6f
Chhavi <chhavi7104@gmail.com>
2026-09-04 02:38:44 +0530

    Update production API URL

--- banking-frontend/src/environments/environment.ts
+++ banking-frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: false,
-  apiBaseUrl: 'http://localhost:8080/api'
+  production: true,
+  apiBaseUrl: 'https://financedashboard-production-8464.up.railway.app/api'
 };
